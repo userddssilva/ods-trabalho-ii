d6881c0920962273a7bc30c0e51d9dd718dad7e6
Douglas Silva <dsm.lic17@uea.edu.br>
2021-07-23 10:52:45 -0400

    feat: API

--- backend/recommender.py
+++ backend/recommender.py
@@ -1,7 +1,8 @@
 import json
 
-f = open('animes_json.json')
+f = open('backend/animes.json')
 animes_json = json.load(f)
+
 
 def manhattan(rating1, rating2):
     distance = 0
@@ -20,17 +21,5 @@ def computeNearestNeighbor(input_anime):
             distance = manhattan(animes_json[anime], animes_json[input_anime])
             distances.append((distance, anime))
     distances.sort()
-    return distances[:10]
-
-def recommend(input_anime, animes):
-    nearest = computeNearestNeighbor(input_anime, animes)[0][1]
-
-    recommendations = []
-    neighborRatings = animes[nearest]
-    animeRatings = animes[input_anime]
-    for artist in neighborRatings:
-        if not artist in animeRatings:
-            recommendations.append((artist, neighborRatings[artist]))
-    return sorted(recommendations, key=lambda artistTuple: artitTuple[1], reverse = True)
-
-print(computeNearestNeighbor("Kimi no Na wa."))
+    map_anime = list(map(lambda x: {"anime": x[1], "distance": x[0]}, distances))
+    return json.dumps(map_anime[:10])
--- server/instace_server.py
+++ server/instace_server.py
@@ -1,11 +1,29 @@
 from flask import Flask
+from backend.connection import Connection
+import backend.recommender as recommender
 
 app = Flask(__name__)
 
+connection = Connection()
 
-@app.route("/teste")
-def index():
-    return 'Olá Mundo!'
+
+@app.route("/create_user/<login>/<password>", strict_slashes=False)
+def create_user(login, password):
+    return connection.createUser(login, password)
+
+
+@app.route("/rate_anime/<iduser>/<anime>/<rate>", strict_slashes=False)
+def rate_anime(iduser, anime, rate):
+    try:
+        connection.rate_anime(anime, iduser, rate)
+        return "Anime avaliado com sucesso"
+    except:
+        return "Ocorreu um erro durante a avalição tente novamente"
+
+
+@app.route("/recommend/<anime>", strict_slashes=False)
+def recommend(anime):
+    return recommender.computeNearestNeighbor(anime)
 
 
 def init_connection():
