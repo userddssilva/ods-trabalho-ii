a640da05a477d7e2a3d58401e18ff99e4a498c6f
Dayvson Silva-DAYVSONS <dayvsons@motorola.com>
2021-07-26 07:59:43 -0400

    update: adiciona tabela ao banco

--- app/backend/connection.py
+++ app/backend/connection.py
@@ -8,18 +8,75 @@ class Connection:
     def __init__(self):
         self.connection = sqlite3.connect('db.db', check_same_thread=False)
         self.create_table_initial()
-        pass
+        # self.populate_animes()
+
 
     def create_table_initial(self):
         self.connection.execute(
             '''CREATE TABLE IF NOT EXISTS User(id text PRIMARY KEY, login text, password text)''')
+        
+        self.connection.execute(
+            '''
+            CREATE TABLE IF NOT EXISTS Anime(
+                id text PRIMARY KEY,
+                name text,
+                drama INTEGER DEFAULT 0,
+                romance INTEGER DEFAULT 0,
+                school INTEGER DEFAULT 0,
+                supernatural INTEGER DEFAULT 0,
+                action INTEGER DEFAULT 0,
+                adventure INTEGER DEFAULT 0,
+                fantasy INTEGER DEFAULT 0,
+                magic INTEGER DEFAULT 0,
+                military INTEGER DEFAULT 0,
+                shounen INTEGER DEFAULT 0,
+                comedy INTEGER DEFAULT 0,
+                historical INTEGER DEFAULT 0,
+                parody INTEGER DEFAULT 0,
+                samurai INTEGER DEFAULT 0,
+                sci_Fi INTEGER DEFAULT 0,
+                thriller INTEGER DEFAULT 0,
+                sports INTEGER DEFAULT 0,
+                super Power INTEGER DEFAULT 0,
+                space INTEGER DEFAULT 0,
+                slice_of_life INTEGER DEFAULT 0,
+                mecha INTEGER DEFAULT 0,
+                music INTEGER DEFAULT 0,
+                mystery INTEGER DEFAULT 0,
+                seinen INTEGER DEFAULT 0,
+                martial_rts INTEGER DEFAULT 0,
+                vampire INTEGER DEFAULT 0,
+                shoujo INTEGER DEFAULT 0,
+                horror INTEGER DEFAULT 0,
+                police INTEGER DEFAULT 0,
+                psychological INTEGER DEFAULT 0,
+                demons INTEGER DEFAULT 0,
+                ecchi INTEGER DEFAULT 0,
+                josei INTEGER DEFAULT 0,
+                shounen_ai INTEGER DEFAULT 0,
+                game INTEGER DEFAULT 0,
+                dementia INTEGER DEFAULT 0,
+                harem INTEGER DEFAULT 0,
+                cars INTEGER DEFAULT 0,
+                kids INTEGER DEFAULT 0,
+                shoujo_ai INTEGER DEFAULT 0,
+                genre INTEGER DEFAULT 0,
+                hentai INTEGER DEFAULT 0,
+                yaoi INTEGER DEFAULT 0,
+                yuri INTEGER DEFAULT 0
+            )
+            '''
+        )
+        
         self.connection.execute(
             '''CREATE TABLE IF NOT EXISTS View_Anime 
             (
             id text PRIMARY KEY, 
-            anime text,user_id text,
+            anime_id text,
+            user_id text,
             rate INTEGER,
-            FOREIGN KEY(user_id) REFERENCES User(id)
+            FOREIGN KEY(user_id) REFERENCES User(id),
+            FOREIGN KEY(anime_id) REFERENCES Anime(id)
             )''')
 
     def getConnection(self):
@@ -36,7 +93,7 @@ class Connection:
         return result.fetchall()
 
     def get_animes_user(self, id_user):
-        res = self.connection.execute("SELECT anime, rate FROM View_Anime WHERE user_id = ?", (id_user,)
+        res = self.connection.execute("SELECT anime_id, rate FROM View_Anime WHERE user_id = ?", (id_user,)
                                       ).fetchall()
         if res is None:
             return [("", "")]
@@ -51,10 +108,34 @@ class Connection:
             return None
         else:
             return {"id": result[0][0]}
+    
+    def get_all_animes(self):
+        result = self.connection.execute("SELECT * FROM Anime")
+        return result.fetchall()
+
+    def get_n_animes(self, n=10):
+        result = self.connection.execute(f"SELECT * FROM Anime LIMIT {n}")
+        return result.fetchall()
+    
+    def populate_animes(self):
+        animes = self.get_all_animes()
+        if len(animes) <= 0:
+            path = os.path.join('app', 'backend')
+            fullpath = os.path.join(path, 'animes.json')
+            with open(fullpath) as json_file:
+                data = json.load(json_file)
+                for key,value in data.items():
+                    m_id = str(uuid.uuid4())
+                    v = list(value.values())
+                    v.insert(0, key)
+                    v.insert(0, m_id)
+                    print(f"##=Values={tuple(v)}")
+                    self.connection.execute('''INSERT INTO Anime VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);''', v)
+                self.connection.commit()
 
     def rate_anime(self, anime, id_user, rate):
         m_id = str(uuid.uuid4())
-        res = self.connection.execute("SELECT * FROM View_Anime WHERE user_id = ? AND anime = ?", (id_user, anime))
+        res = self.connection.execute("SELECT * FROM View_Anime WHERE user_id = ? AND anime_id = ?", (id_user, anime))
         if len(res.fetchall()) == 0:
             print("Criando Avaliacao")
             self.connection.execute("INSERT INTO View_Anime VALUES (?, ?, ?, ?);", (m_id, anime, id_user, rate))
@@ -68,7 +149,7 @@ class Connection:
 
     def get_json(self):
         res = self.connection.execute(
-            '''SELECT login, anime, rate FROM User as us INNER JOIN 
+            '''SELECT login, anime_id, rate FROM User as us INNER JOIN 
             View_Anime as va on us.id = va.user_id;''').fetchall()
 
         map_users = dict()
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+from flask import redirect, url_for, request
 from .backend.connection import Connection
 from .backend import recommender
 
 app = Flask(__name__)
 
 connection = Connection()
-
+connection.populate_animes()
 
 @app.route("/")
 def index():
@@ -16,17 +17,27 @@ def index():
 def login():
     login = request.form["email"]
     password = request.form["password"]
-    print(connection.get_user(login, password))
     user = connection.get_user(login, password)
     if user is not None:
-        print('User found')
+        animes = connection.get_animes_user(user['id'])
+        if len(animes) <= 0:
+            return redirect(url_for('first_login', user_id=user['id']))
         return render_template("home.html")
     else:
         return render_template("login.html")
 
 
+@app.route("/first_login/<user_id>")
+def first_login(user_id=None):
+    print(f"##=user_id={user_id}")
+    # recommendations = connection.get_json()
+    recommendations = connection.get_n_animes()
+    print(f"##=recomendations={recommendations}")
+    return render_template("first-login.html", recommends=recommendations)
+
 @app.route("/sign_up")
 def sign_up():
+    print(connection.get_all_animes())
     return render_template("sign-up.html")
 
 
